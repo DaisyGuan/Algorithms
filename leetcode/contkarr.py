51c1dad38b077122fa4fdf0e8a297b7d55a609bb
DaisyGuan <orchiaguan@gmail.com>
2017-10-07 20:36:56 -0400

    update

--- leetcode/contkarr.py
+++ leetcode/contkarr.py
@@ -1,9 +1,9 @@
 def contkarr(A,k):
     maxSum = 0
-    currSum = sum(A[0:k+1])
-    for i in range(1,len(A)-k):
+    currSum = sum(A[0:k])
+    for i in range(0,len(A)-k):
         currSum = currSum - A[i] + A[i+k]
         maxSum=max(maxSum, currSum)
     return maxSum
 
-print contkarr([1,2,3,4,5,1,100], 4)
+print contkarr([1,2,3,4,5,1,100], 3)
