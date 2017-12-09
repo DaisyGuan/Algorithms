fb34dafed8d1c92c104ae018977e903a2697e061
DaisyGuan <orchiaguan@gmail.com>
2017-12-09 17:52:52 -0500

    update

--- FBmj/EncodingDecoding64.py
+++ FBmj/EncodingDecoding64.py
@@ -1,41 +1,35 @@
+#BASE64
 import base64
-encoded = base64.b64encode("")
-print encoded
+encoded = base64.b64encode("absdsdfa")
+#print encoded
 data = base64.b64decode(encoded)
-print data
-s = 'haha/gdfsa/dfaf'
-print s.split('/')
+#print data
+import binascii
+#print binascii.b2a_base64('absdsdfa')
+#Convert binary data to a line of ASCII characters in base64 coding.
+#print binascii.a2b_base64('YWJzZHNkZmE=')
+#Convert a block of base64 data back to binary and return the binary data. More than one line may be passed at a time.
 
-["X:Z;Y_","RtbB9G(P","On?>","{y,9f~wgL"]
+import urllib2
+def urlencode(s):
+    return urllib2.quote(s)
+def urldecode(s):
+    return urllib2.unquote(s).decode('utf8')
 
-import base64
-class Codec:
+result = urlencode('https://gist.github.com/renyi/ ')
+#print result
+#print urldecode(result)
 
-    def encode(self, strs):
-        """Encodes a list of strings to a single string.
-
-        :type strs: List[str]
-        :rtype: str
-        """
-        res = ''
-        for s in strs:
-            res += base64.b64encode(s) +'/'
-        return res
-
-
-    def decode(self, s):
-        """Decodes a single string to a list of strings.
-
-        :type s: str
-        :rtype: List[str]
-        """
-        res = s.split('/')
-
-        for i in range(len(res)):
-            res[i] = base64.b64decode(res[i])
-        return res
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+#binary to hex
+#print hex(int('10000011101000011010100010010111', 2))
+#Hex to binary
+#print bin(0x83a1a897)
+print "absdsdfa".encode("base64")
+print "YWJzZHNkZmE=".decode('base64')
+print "https://gist.github.com/renyi/ ".encode('url')
+#Perform hexbin4 binary-to-ASCII translation and return the resulting string.
+#The argument should already be RLE-coded, and have a length divisible by 3 (except possibly the last fragment).
+#print binascii.a2b_uu('0101110')
+#Convert a single line of uuencoded data back to binary and return the binary data.
+#Lines normally contain 45 (binary) bytes, except for the last line. Line data may be followed by whitespace.
+#print binascii.b2a_uu('EQE')
