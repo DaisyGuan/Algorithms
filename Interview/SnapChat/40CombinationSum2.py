cfa8bff566c545cd43a581100de2893a48d8baa1
DaisyGuan <orchiaguan@gmail.com>
2018-01-10 16:06:22 -0500

    update

---
+++ Interview/SnapChat/40CombinationSum2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        return self.search(candidates, 0 ,target)
+
+    def search(self, candidates, start, target):
+        if target==0:
+            return [[]]
+        res=[]
+        for i in xrange(start,len(candidates)):
+            if i!=start and candidates[i]==candidates[i-1]:
+                continue
+            if candidates[i]>target:
+                break
+            for r in self.search(candidates, i+1, target-candidates[i]):
+                res.append([candidates[i]]+r)
+        return res
+
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        dp = [set() for i in xrange(target+1)]
+        dp[0].add(())
+        for num in candidates:
+            for t in xrange(target, num-1, -1):
+                for prev in dp[t-num]:
+                    dp[t].add(prev + (num,))
+        return list(dp[-1])
