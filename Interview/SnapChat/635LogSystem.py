dd0a173e609b98db0a9cb2ec8549cbe28106ee64
DaisyGuan <orchiaguan@gmail.com>
2018-01-11 13:38:31 -0500

    update

--- Interview/SnapChat/635LogSystem.py
+++ Interview/SnapChat/635LogSystem.py
@@ -38,3 +38,20 @@ class LogSystem(object):
 # obj = LogSystem()
 # obj.put(id,timestamp)
 # param_2 = obj.retrieve(s,e,gra)
+
+"""
+You are given several logs that each log contains a unique id and timestamp.
+Timestamp is a string that has the following format: Year:Month:Day:Hour:Minute:Second,
+for example, 2017:01:01:23:59:59. All domains are zero-padded decimal numbers.
+
+Design a log storage system to implement the following functions:
+
+void Put(int id, string timestamp):
+Given a log's unique id and timestamp, store the log in your storage system.
+
+int[] Retrieve(String start, String end, String granularity):
+Return the id of logs whose timestamps are within the range from start to end.
+Start and end all have the same format as timestamp.
+However, granularity means the time level for consideration.
+For example, start = "2017:01:01:23:59:59", end = "2017:01:02:23:59:59",
+granularity = "Day", it means that we need to find the logs within the range from Jan. 1st 2017 to Jan. 2nd 2017.
